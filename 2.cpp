55465173329987eb6ac69bc26354f4862d31f18b
July-jsy <930728875@qq.com>
2025-03-07 16:24:25 +0800

    second

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    bool a;
+    cout << sizeof(bool) << " " << sizeof(a) << endl;
+    a = 19;
+    cout << "a is " << a << endl;
+    cout << boolalpha << a << endl;
+    cout << noboolalpha << a << endl;
+    return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+typedef struct BookName
+{
+    char bookname[20];
+    char author[10];
+    float price;
+    void print()
+    {
+        cout << bookname << " "
+             << author << " "
+             << price << endl;
+    }
+} Book;
+
+int main(int argc, char const *argv[])
+{
+    Book book = {0};
+    strcpy(book.bookname, "计算机网络");
+    strcpy(book.author, "lisi");
+    book.price = 19.9;
+    book.print();
+    strcpy(book.bookname, "aaa");
+    book.print();
+    Book *p = &book;
+    p->print();
+    Book b2;
+    b2 = book;
+    b2.print();
+    return 0;
+}
