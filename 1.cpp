01336b28621c4ab0be3c6ca10a55de191d5af26f
July-jsy <930728875@qq.com>
2025-03-07 15:40:51 +0800

    first

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+namespace teacher
+{
+    // 变量,函数,结构体
+    char name[10] = "rose";
+    int age = 14;
+    void showname()
+    {
+        cout << name << endl;
+    }
+}
+namespace worker
+{
+    char name[10] = "lihua";
+    int age = 12;
+    void showname()
+    {
+        cout << name << endl;
+    }
+}
+int age = 12;
+using namespace teacher;
+using namespace worker;
+int main(int argc, char const *argv[])
+{
+    int age = 11;
+    teacher::showname();
+    cout << age << endl
+         << teacher::age << endl
+         << ::age << endl;
+
+    return 0;
+}
